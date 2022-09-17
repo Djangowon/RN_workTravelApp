52f5020415dac6b3d33c0dbbf9affda735c4aa91
DowonJang <rosewoodowon@gmail.com>
2022-09-17 23:25:55 +0900

    1. init, header

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <SafeAreaView>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Text style={styles.btnText}>Work</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.btnText}>Travel</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +32,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    merginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+    color: "white",
   },
 });
---
+++ colors.js
@@ -0,0 +1,4 @@
+export const theme = {
+  bg: "black",
+  gre: "#3A3D40",
+};
